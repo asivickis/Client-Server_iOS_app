612370b9962c4b5f24e7da00282fc3609aa398a2
Aleksandrs Sivickis <biggy.man747@gmail.com>
2021-10-18 00:35:45 +0300

    Initial project

---
+++ Client_server_lessons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Client_server_lessons
+//
+//  Created by Aleksandrs Sivickis on 18/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
